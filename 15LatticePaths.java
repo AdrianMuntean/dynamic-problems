4f8bae7249f3c8d62bbede1fc0992f34d5ca12ee
Adrian Muntean <adrian.muntean@ateliere.com>
2021-10-21 09:07:28 +0300

    Add grid traveler

---
+++ 15LatticePaths.java
@@ -0,0 +1,62 @@
+import java.math.BigInteger;
+
+
+/*
+  Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+  This is a variation of the grid traveler problem in the way that the grid is square.
+*/
+
+public class LatticePaths {
+    long noPaths = 0;
+
+    /*
+    Simple solution with backtracking, but it's taking way too much time
+    */
+    void latticePathsWithDir(int gridSize, int indexI, int indexJ) {
+        if (indexI > gridSize || indexJ > gridSize) {
+            return;
+        }
+
+        if (indexI == gridSize && indexJ == gridSize) {
+            noPaths++;
+            return;
+        }
+
+        latticePathsWithDir(gridSize, indexI + 1, indexJ); // LEFT
+        latticePathsWithDir(gridSize, indexI, indexJ + 1); // DOWN
+
+    }
+
+    void latticePaths(int gridSize) {
+        latticePathsWithDir(gridSize, 0, 0);
+        return;
+    }
+
+
+    /*
+    Use Pascal's triangle and the formula of binomial coefficient: (2 * gridSize)! / gridSize!^2
+     */
+    private long latticePathsWithMath(int gridSize) {
+        BigInteger factOfDouble = BigInteger.ONE;
+        BigInteger nFact = BigInteger.ONE;
+        for (int i = 2; i <= gridSize * 2; i++) {
+            factOfDouble = factOfDouble.multiply(BigInteger.valueOf(i));
+            if (i <= gridSize) {
+                nFact = nFact.multiply(BigInteger.valueOf(i));
+            }
+        }
+
+        return factOfDouble.divide(nFact.multiply(nFact)).longValue();
+    }
+
+    public static void main(String[] args) {
+        LatticePaths latticePaths = new LatticePaths();
+        System.out.println(latticePaths.latticePathsWithMath(20));
+//         latticePaths.latticePaths(20);
+//        System.out.println(latticePaths.noPaths);
+
+    }
+
+
+}
---
+++ gridTraveler.js
@@ -0,0 +1,52 @@
+/*
+    Say that you are a traveler on a 2D grid. You begin in the top-left corner and your goal is to
+    travel to the bottom-right corner. You may only move down or right. 
+
+    In how many ways can you travel to the goal on a grid with dimensions m * n.
+
+    ex: 
+      gridTraveler(1, 1) => 1
+      gridTraveler(2, 3) => 3
+      gridTraveler(3, 3) => 6
+
+    How to try and solve this?
+     - start with an example. Not a trivial one and not a really big one. eg. gridTraveler(3, 3)
+     - try and decrease the problem size. You can reuse past computations.
+     - try to visualize a breakdown of the problem using a tree.
+         (3,3)
+	/     \
+      (2,3)  (3,2)
+     /   \
+   (1,3) (2,2)
+  /   \
+(0,3)# (1,2)
+          \
+	 (1,1) => this returns 1  
+# returns 0
+*/
+
+// Classic implementation. 2**(n+m) complexity.
+const gridTraveler = (m, n) => {
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+
+    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+
+};
+
+const gridTravelerMemo = (m, n, memo={}) => {
+    const key = `${m}|${n}`;
+    if (key in memo) return memo[key];
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+   
+    memo[key] = gridTravelerMemo(m - 1, n, memo) + gridTravelerMemo(m, n - 1, memo);
+    return memo[key];
+}
+
+// console.log(gridTraveler(20, 20)); => too much time
+console.log(gridTravelerMemo(21, 21)); // 137846528820 
+
+
+
+
